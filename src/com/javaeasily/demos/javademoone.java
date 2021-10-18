ada403835c3c1b3af840bb3d0124544017bab4b5
androidlion48 <clarence.moore1992@gmail.com>
2021-10-17 17:34:56 -0700

    full JAVA demo

---
+++ src/com/javaeasily/demos/javademoone.java
@@ -0,0 +1,13 @@
+ package com.javaeasily.demos;
+
+public class javademoone {
+
+	public static void main(String[] args) {
+		sayTitle(" Free Code Camp");
+	}
+	private static void sayTitle(String name) {
+	System.out.println("JAVA Demo Trial."+ name);	// TODO Auto-generated method stub
+
+	}
+
+}
